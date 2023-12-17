e73ca9f1168934c04f183b35fd71300a72cdaab5
Le Tuan Dan <letuandan_t66@hus.edu.vn>
2023-12-17 18:01:09 +0700

    56

--- Frontend-React.JS/src/services/userService.js
+++ Frontend-React.JS/src/services/userService.js
@@ -21,6 +21,10 @@ const editUserService = (inputData) => {
     return axios.put('/api/edit-user', inputData)
 }
 
+const getAllCodeService = (inputType) => {
+    return axios.get(`/api/allcode?type=${inputType}`)
+}
 
-export { handleLoginApi, getAllUsers, createNewUserSevice, deleteUserService, editUserService }
+
+export { handleLoginApi, getAllUsers, createNewUserSevice, deleteUserService, editUserService, getAllCodeService }
 
