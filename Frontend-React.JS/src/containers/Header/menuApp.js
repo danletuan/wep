66cbd33771eb12d8d564441a83599e157822aa50
unknown <namyhsd1@gmail.com>
2023-12-20 21:58:43 +0700

    71

--- Frontend-React.JS/src/containers/Header/menuApp.js
+++ Frontend-React.JS/src/containers/Header/menuApp.js
@@ -12,9 +12,13 @@ export const adminMenu = [
             {
                 name: 'menu.admin.manage-doctor', link: '/system/manage-doctor'
             },
-            {
-                name: 'menu.admin.manage-admin', link: '/system/user-admin'
-            },
+            // {
+            //     name: 'menu.admin.manage-admin', link: '/system/user-admin'
+            // },
+            { //quản lý kế hoạch khám bệnh
+
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+            }
         ]
 
 
@@ -44,4 +48,19 @@ export const adminMenu = [
             // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
         ]
     },
+
+];
+
+export const doctorMenu = [
+    {
+        name: 'menu.admin.manage-user',
+        menus: [
+            { //quản lý kế hoạch khám bệnh
+
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+
+            }]
+    }
+
+
 ];
